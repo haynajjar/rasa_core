7fd6650f8b338ed9626eebb84abb46896d7a8f6d
Mihir Patel <ee.mihir.patel@gmail.com>
2018-01-06 15:51:13 -0500

    add docker support with entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM python:2.7-slim
+
+# Run updates, install basics and cleanup
+# - build-essential: Compile specific dependencies
+# - git-core: Checkout git repos
+RUN apt-get update -qq \
+    && apt-get install -y --no-install-recommends build-essential git-core openssl libssl-dev libffi6 libffi-dev curl  \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+
+# use bash always
+RUN rm /bin/sh && ln -s /bin/bash /bin/sh
+
+# Default to UTF-8 file.encoding
+ENV LANG C.UTF-8
+
+# workdir
+WORKDIR /app
+COPY . /app
+
+# rasa stack
+## rasa nlu
+RUN pip install -r requirements/requirements_docker.txt
+## spacy models
+RUN pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-1.2.0/en_core_web_sm-1.2.0.tar.gz --no-cache-dir > /dev/null \
+    && python -m spacy link en_core_web_sm en
+## rasa core
+RUN pip install rasa_core
+
+# volumes
+VOLUME ["/app/logs", "/app/data", "/app/config"]
+
+
+EXPOSE 5000
+
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+function print_help {
+    echo "Available options:"
+    echo " help                                                     - Print this help"
+    echo " download {mitie, spacy en, spacy de}                     - Download packages for mitie or spacy (english or german)"
+    echo " run                                                      - Run an arbitrary command inside the container"
+    echo " start-nlu-server commands (rasa cmd line arguments)      - Start RasaNLU server"
+    echo " start-nlu-server -h                                      - Print RasaNLU help"
+    echo " train-rasa-core commands (rasa core cmd line arguments)  - Train RasaCore"
+    echo " run-rasa-core commands (rasa core cmd line arguments)    - Run RasaCore"
+}
+
+function download_package {
+    case $1 in
+        mitie)
+            echo "Downloading mitie model..."
+            python -m rasa_nlu.download -p mitie
+            ;;
+        spacy)
+            case $2 in 
+                en|de)
+                    echo "Downloading spacy.$2 model..."
+                    python -m spacy download "$2"
+                    echo "Done."
+                    ;;
+                *) 
+                    echo "Error. Rasa_nlu supports only english and german models for the time being"
+                    print_help
+                    exit 1
+                    ;;
+            esac
+            ;;
+        *) 
+            echo "Error: invalid package specified."
+            echo 
+            print_help
+            ;;
+    esac
+}
+
+case ${1} in
+    download)
+        download_package ${@:2}
+        ;;
+    run)
+        exec "${@:2}"
+        ;;
+    start-nlu-server)
+        exec python -m rasa_nlu.server "${@:2}"
+        ;;
+    train-rasa-core)
+        exec python -m rasa_core.train "${a:2}"
+        ;;
+    run-rasa-core)
+        exec python -m rasa_core.run "${a:2}"
+        ;;
+    *)
+        print_help
+        ;;
+esac
+
